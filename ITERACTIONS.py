86e4484bd242e5e1a87f874f4fb9b6504a4a443a
Eugene <35332515+zhexyez@users.noreply.github.com>
2020-01-26 07:00:59 +0200

    Minor improvements to changing maps by the door
    Now when you interact the door, it will place you in front of relative door on the other map.

--- ITERACTIONS.py
+++ ITERACTIONS.py
@@ -7,9 +7,37 @@ import copy
 
 class ITERACTS:
 
+    def change_map_player_position (is_right, relative_half):
+        if is_right == 0:
+            for e in range (0, DB.DRAW.X_get_map-1):
+                for i in range (0, DB.DRAW.Y_get_map-1):
+                    if DB.DRAW.get_map_points[e][i] == 5:
+                        for column_pos in range (0, DB.DRAW.X_get_map-1):
+                            for row_pos in range (0, DB.DRAW.Y_get_map-1):
+                                if DB.DRAW.get_map_points[column_pos][row_pos] == 7:
+                                    DB.DRAW.get_map_points[column_pos][row_pos] = 2
+                                    DB.DRAW.get_map_points[e][i+1] = 7
+                                    return None
+        elif is_right == 1:
+            for e in range (DB.DRAW.X_get_map-1, 0, -1):
+                for i in range (DB.DRAW.Y_get_map-1, 0, -1):
+                    if DB.DRAW.get_map_points[e][i] == 5:
+                        for column_pos in range (DB.DRAW.X_get_map-1, 0, -1):
+                            for row_pos in range (DB.DRAW.Y_get_map-1, 0, -1):
+                                if DB.DRAW.get_map_points[column_pos][row_pos] == 7:
+                                    DB.DRAW.get_map_points[column_pos][row_pos] = 2
+                                    DB.DRAW.get_map_points[e][i-1] = 7
+                                    return None
+
+    def send_to_redefine_map_points ():
+        set_map = MAPP.MAPS.read_map(DB.DRAW.maps_db[DB.DRAW.current_map])
+        get_map = set_map
+        DB.DRAW.X_get_map = len(get_map)
+        DB.DRAW.Y_get_map = len(get_map[0])
+        DB.DRAW.get_map_points = DR.DRAWS.map_analyzer (get_map)
+
     @staticmethod
     def is_iteracted (player_current, object_current, current_X_position, flag):
-        #print(player_current, object_current)
         if object_current == 8:
             # object is "o" coin/money
             GSTAT.GAMESTATS.player_stats["money"] += 10
@@ -28,39 +56,27 @@ class ITERACTS:
                 
                 if current_X_position <= even_less_than_half:
                     DB.DRAW.current_map -= 1
-                    set_map = MAPP.MAPS.read_map(DB.DRAW.maps_db[DB.DRAW.current_map])
-                    get_map = set_map
-                    DB.DRAW.X_get_map = len(get_map)
-                    DB.DRAW.Y_get_map = len(get_map[0])
-                    DB.DRAW.get_map_points = DR.DRAWS.map_analyzer (get_map)
+                    ITERACTS.send_to_redefine_map_points()
+                    ITERACTS.change_map_player_position(1, even_more_than_half)
                     return player_current, 2, current_X_position, True
                 
                 elif current_X_position >= even_more_than_half:
                     DB.DRAW.current_map += 1
-                    set_map = MAPP.MAPS.read_map(DB.DRAW.maps_db[DB.DRAW.current_map])
-                    get_map = set_map
-                    DB.DRAW.X_get_map = len(get_map)
-                    DB.DRAW.Y_get_map = len(get_map[0])
-                    DB.DRAW.get_map_points = DR.DRAWS.map_analyzer (get_map)
+                    ITERACTS.send_to_redefine_map_points()
+                    ITERACTS.change_map_player_position(0, even_more_than_half)
                     return player_current, 2, current_X_position, True
             # if odd
             else:
                 rounded_float = round(Y_get_map_copy/2)
                 if current_X_position <= rounded_float:
                     DB.DRAW.current_map -= 1
-                    set_map = MAPP.MAPS.read_map(DB.DRAW.maps_db[DB.DRAW.current_map])
-                    get_map = set_map
-                    DB.DRAW.X_get_map = len(get_map)
-                    DB.DRAW.Y_get_map = len(get_map[0])
-                    DB.DRAW.get_map_points = DR.DRAWS.map_analyzer (get_map)
+                    ITERACTS.send_to_redefine_map_points()
+                    ITERACTS.change_map_player_position(1, rounded_float)
                     return player_current, 2, current_X_position, True
                 elif current_X_position >= (rounded_float + 2):
                     DB.DRAW.current_map += 1
-                    set_map = MAPP.MAPS.read_map(DB.DRAW.maps_db[DB.DRAW.current_map])
-                    get_map = set_map
-                    DB.DRAW.X_get_map = len(get_map)
-                    DB.DRAW.Y_get_map = len(get_map[0])
-                    DB.DRAW.get_map_points = DR.DRAWS.map_analyzer (get_map)
+                    ITERACTS.send_to_redefine_map_points()
+                    ITERACTS.change_map_player_position(0, rounded_float+2)
                     return player_current, 2, current_X_position, True
                 
         elif object_current == 9:
